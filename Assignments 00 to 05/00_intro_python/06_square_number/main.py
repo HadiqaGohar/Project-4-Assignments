d44712b28416d9b864046bd8dbebe49bfb7ee696
HadiqaGohar <160311280+HadiqaGohar@users.noreply.github.com>
2025-03-21 02:09:57 +0500

    Add files via upload

---
+++ Assignments 00 to 05/00_intro_python/06_square_number/main.py
@@ -0,0 +1,8 @@
+def main():
+    # Prompt user for a number and convert to float
+    num = float(input("Type a number to see its square: "))
+
+    print(f"{num} squared is {num ** 2}")
+
+if __name__ == '__main__':
+    main()
